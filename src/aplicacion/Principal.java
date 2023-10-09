5220d2a74045a70ba4964ecafabea30040cfb7b6
FLAVIA MENDEZ TSUTSUMI <flaviaestefany.mendeztsutsumi@usp.ceu.es>
2023-10-09 11:48:16 +0200

    Primer programa

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,8 @@
+package aplicacion;
+import java.util.*;
+import presentacion.InterfazUsuario;
+public class Principal{
+	public static void main(String[] args){ 
+		InterfazUsuario.ejecutar(args);
+	}
+}
